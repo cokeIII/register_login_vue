c87cd9785e35c57d4dbefc8fcfba7d895edc95d3
cokeIII <coke2484@gmail.com>
2021-12-03 15:45:42 +0700

    f

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>{{ texts }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      texts: "login",
+    };
+  },
+};
+</script>
---
+++ src/components/register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "register",
+  data() {
+    return {
+      text: "register",
+    };
+  },
+};
+</script>
